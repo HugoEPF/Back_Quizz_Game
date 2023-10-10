495e31dcaabc967a31db80eba8a242f904e8e62c
= <=>
2023-10-10 10:19:06 +0200

    Premier commit

---
+++ src/main/java/com/example/quizzgamepeuf/QuizzGamePeufApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizzgamepeuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzGamePeufApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzGamePeufApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quizzgamepeuf/QuizzGamePeufApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizzgamepeuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzGamePeufApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
