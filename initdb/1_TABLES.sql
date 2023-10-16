761bdac9261e01ba2d965b0f5835250e8ae9dd87
Jules <jules.alfonsi@epfedu.fr>
2023-10-16 10:22:43 +0200

    initialisation bdd

---
+++ initdb/1_TABLES.sql
@@ -0,0 +1,29 @@
+create table "user"
+(
+    id SERIAL PRIMARY KEY,
+    pseudo VARCHAR(255) not null,
+    email VARCHAR(255) not null,
+    score INTEGER
+);
+
+create table Admin
+(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(255) not null,
+    prenom VARCHAR(255) not null,
+    email VARCHAR(255) not null
+);
+
+create table Question
+(
+    id SERIAL PRIMARY KEY,
+    contenu VARCHAR(255) not null,
+    genre VARCHAR(255) not null
+);
+
+create table Quizz
+(
+    id SERIAL PRIMARY KEY,
+    nombre_questions INTEGER not null,
+    genre VARCHAR(255) not null
+);
---
+++ initdb/2_DEFAULT_ENTRIES.sql
@@ -0,0 +1,21 @@
+INSERT INTO "user" (id, pseudo, email, score) VALUES (1, 'julesalf', 'jules.alfonsi@epfedu.fr', 0);
+
+INSERT INTO admin (id, nom, prenom, email) VALUES (1, 'fontaine', 'hugo', 'hugo.fontaine@epfedu.fr');
+
+INSERT INTO question (id, contenu, genre) VALUES (1, 'Quel est le félin le plus rapide au monde?', 'Culture générale');
+
+INSERT INTO quizz (id, nombre_questions, genre) VALUES (1, 10, 'Culture générale');
+
+INSERT INTO "user" (id, pseudo, email, score) VALUES (2, 'thib', 'thibault.collet@epfedu.fr', 0);
+
+INSERT INTO admin (id, nom, prenom, email) VALUES (2, 'bonnefoy', 'paulin', 'paulin.bonnefoy@epfedu.fr');
+
+INSERT INTO question (id, contenu, genre) VALUES (2, 'Quel pays a gagné le mondial de foot en 2014', 'Sport');
+
+INSERT INTO quizz (id, nombre_questions, genre) VALUES (2, 10, 'Sport');
+
+INSERT INTO "user" (id, pseudo, email, score) VALUES (3, 'victorsal', 'victor.salles@epfedu.fr', 0);
+
+INSERT INTO question (id, contenu, genre) VALUES (3, 'De quel groupe faisait partie beyonce?', 'Musique');
+
+INSERT INTO quizz (id, nombre_questions, genre) VALUES (3, 10, 'Musique');
